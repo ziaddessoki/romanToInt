fca8b807efca1463c5750b4bfe48bc3748660c7f
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-07-22 22:07:07 +0000

    o(n)

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 //Swtch from Roman to Integer
 
+const romanToInt = (input)=>{
+  let arr = input.split('')
+  let output= 0;
+  for(let i =0;i<arr.length;i++){
+    switch(arr[i]){
+      case'I':
+        if(arr[i+1]==='V'){output += 4;}
+        else if (arr[i+1]==='X'){output += 9;}
+        else{output += 1;}
+        break;
+      case 'V':
+        output +=5;
+        break; 
+      case'X':
+        if(arr[i+1]==='L'){output += 40;}
+        else if (arr[i+1]==='C'){output += 90;}
+        else{output += 10;}
+      break; 
+      default:
+        total += 1000;
+        break;  
+    }
+ }return output;
+}
+
+romanToInt('XXXIII')
